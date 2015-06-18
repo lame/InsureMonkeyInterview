9c5fce7af3cf5fa6d4edc569d8b50517cf93ed1e
Ryan Kuhl <rkk09c@gmail.com>
2015-06-17 20:41:51 -0400

    comma separated values done

--- unit_test.py
+++ unit_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from flatten import Flatten
+from csv import CommaSeparatedValues
 
 
 class Test_Scripts(unittest.TestCase):
@@ -17,6 +18,12 @@ class Test_Scripts(unittest.TestCase):
         self.assertEqual(Flatten.flatten_list([['a', 'b'], 'a', ['a', 'b', ], ['a'], 'b']),
                          ['a', 'b', 'a', 'a', 'b', 'a', 'b'])
 
+    def test_comma_separated_values(self):
+        self.assertEqual(CommaSeparatedValues(3456).output, '3,456')
+        self.assertEqual(CommaSeparatedValues(987654321).output, '987,654,321')
+        self.assertEqual(CommaSeparatedValues(20).output, '20')
+        self.assertEqual(CommaSeparatedValues(12.3).output, '12.3')
+        self.assertEqual(CommaSeparatedValues(3456.3).output,  '3,456.3')
 
 if __name__ == '__main__':
     unittest.main()
